ac8694789d47377b6ec74d03af9c21c676bda970
Carl Sebastian Broberg <cabr2300@cabr2300@student.miun.se>
2025-10-14 13:10:43 +0700

    changed name of test class

--- src/test/java/com/dinosaur/dinosaurexploder/model/TestBombComponent.java
+++ src/test/java/com/dinosaur/dinosaurexploder/model/TestBombComponent.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BombComponentTest {
+public class TestBombComponent {
 
     private BombComponent bombComponent;
 
