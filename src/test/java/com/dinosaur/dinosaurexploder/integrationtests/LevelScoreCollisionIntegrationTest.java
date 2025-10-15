bff65e355825b59e52a4a07fbfd610055ae5de6c
MarcusEstensson2 <maes2300@student.miun.se>
2025-10-15 15:53:31 +0700

    Rename

--- src/test/java/com/dinosaur/dinosaurexploder/integrationtests/LevelScoreCollisionIntegrationTest.java
+++ src/test/java/com/dinosaur/dinosaurexploder/integrationtests/LevelScoreCollisionIntegrationTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LevelIntegrationTest {
+class LevelScoreCollisionIntegrationTest {
 
     private LevelManager levelManager;
     private CollisionHandler collisionHandler;
